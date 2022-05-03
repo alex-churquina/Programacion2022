3a514b57f2c09538b1f7e9e5881649236f1a4223
alex-churquina <104175773+alex-churquina@users.noreply.github.com>
2022-05-02 23:45:09 -0300

    practica leandro2

--- src/practica3.ts
+++ src/practica3.ts
@@ -2,17 +2,16 @@ let numero1: number = Number(prompt("primer dato"));
 let numero2: number = Number(prompt("segundo dato"));
 let suma = 0;
 
-if(numero1<numero2){
-  for(let indice1: number = numero1;indice1<=numero2;indice1++)
-  suma = suma + indice1
-  console.log(suma);
-
-
-}else{
-  for(let indice2: number = numero2; indice2<=numero1;indice2++){
-  suma = suma + indice2;
-  console.log(suma);
-
+if (numero1 < numero2) {
+  for (let indice1: number = numero1; indice1 <= numero2; indice1++) {
+    suma = suma + indice1;
   }
 
+  console.log("El resultado de la suma Normal es:" + suma);
+} else {
+  for (let indice2: number = numero2; indice2 <= numero1; indice2++) {
+    suma = suma + indice2;
+  }
+
+  console.log("El resultado de la suma Invertida es:" + suma);
 }
