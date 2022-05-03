c06b3ec1658ae37c559de2433402f225918fcc01
alex-churquina <104175773+alex-churquina@users.noreply.github.com>
2022-05-02 22:18:39 -0300

    practico Leandro

---
+++ src/practica3.ts
@@ -0,0 +1,18 @@
+let numero1: number = Number(prompt("primer dato"));
+let numero2: number = Number(prompt("segundo dato"));
+let suma = 0;
+
+if(numero1<numero2){
+  for(let indice1: number = numero1;indice1<=numero2;indice1++)
+  suma = suma + indice1
+  console.log(suma);
+
+
+}else{
+  for(let indice2: number = numero2; indice2<=numero1;indice2++){
+  suma = suma + indice2;
+  console.log(suma);
+
+  }
+
+}
